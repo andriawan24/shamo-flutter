3a9bb7e5f95544b32e1d023c2bfaa9e82c9c673b
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2022-03-14 19:02:05 +0700

    Update project

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:shamo/model/cart_model.dart';
-import 'package:shamo/model/user_model.dart';
 import 'package:http/http.dart' as http;
 
 class TransactionService {
